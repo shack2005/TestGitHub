d64d1631d81f177fce75522d14941c079c812ea6
shack2005 <shack2005@gmail.com>
2015-09-29 08:59:17 -0500

    Testing git

---
+++ WebApplication2/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+using WebApplication2;
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
